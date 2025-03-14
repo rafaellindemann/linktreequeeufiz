7811d16b3e48c0560620c653b01884adb69abe5b
rafaellindemann <rafaelld@gmail.com>
2025-03-14 18:16:45 -0300

    att cores dos ícones

--- src/data/links.jsx
+++ src/data/links.jsx
@@ -14,27 +14,27 @@ export const links = [
     link: "https://jamanjo.com.br",
     color: "#242",
     expirationDate: "2028-12-31", // Link expirado
-    icon: <GiFruitTree />,
+    icon: <GiFruitTree size={28} color="#0f0"/>,
   },
   {
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/rafael-lindemann-duarte-59437867/",
     color: "#334",
     expirationDate: "2028-12-31",
-    icon: <FaLinkedin />,
+    icon: <FaLinkedin size={24} color="#bde0fe"/>,
   },
   {
     name: "Instagram",
     link: "https://www.instagram.com/rafael_lambari/",
     color: "#633",
     expirationDate: "2028-12-31",
-    icon: <FaInstagram />,
+    icon: <FaInstagram size={24} color="#ffafcc" />,
   },
   {
     name: "Moto Grupo Lambaris",
     link: "https://www.facebook.com/MotoGrupoLambaris",
     color: "#222",
     expirationDate: "2028-12-31",
-    icon: <GiFishCorpse />,
+    icon: <GiFishCorpse size={24} color="#fdd" />,
   },
 ];
