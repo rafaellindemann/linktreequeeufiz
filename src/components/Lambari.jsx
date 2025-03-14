3a418a1f333a1c49dc9d9afc773cc8ddb9234a54
rafaellindemann <rafaelld@gmail.com>
2025-03-14 20:09:57 -0300

    add lambari

---
+++ src/components/Lambari.jsx
@@ -0,0 +1,10 @@
+import { img } from 'framer-motion/client'
+import React from 'react'
+
+function Lambari() {
+  return (
+    <img src="/irabmal.jpg" alt="Lambari" style={{ height: "24px",    verticalAlign: "middle" }}/>
+  )
+}
+
+export default Lambari
--- src/data/links.jsx
+++ src/data/links.jsx
@@ -1,5 +1,8 @@
+import { span } from "framer-motion/client";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { GiFishCorpse, GiFruitTree } from "react-icons/gi";
+import { RiMotorbikeFill } from "react-icons/ri";
+import Lambari from "../components/Lambari";
 
 export const links = [
   {
@@ -24,17 +27,19 @@ export const links = [
     icon: <FaLinkedin size={24} color="#bde0fe"/>,
   },
   {
+    name: "Moto Grupo Lambaris",
+    link: "https://www.facebook.com/MotoGrupoLambaris",
+    color: "#000",
+    expirationDate: "2028-12-31",
+    icon: <span><Lambari /></span>,
+    // icon: <span><Lambari /><RiMotorbikeFill size={24} color="#fdd"/></span>,
+    // icon: <span><Lambari /><RiMotorbikeFill size={24} color="#fdd"/><GiFishCorpse size={24} color="#fdd" /></span>,
+  },
+  {
     name: "Instagram",
     link: "https://www.instagram.com/rafael_lambari/",
     color: "#633",
     expirationDate: "2028-12-31",
     icon: <FaInstagram size={24} color="#ffafcc" />,
-  },
-  {
-    name: "Moto Grupo Lambaris",
-    link: "https://www.facebook.com/MotoGrupoLambaris",
-    color: "#222",
-    expirationDate: "2028-12-31",
-    icon: <GiFishCorpse size={24} color="#fdd" />,
   },
 ];
